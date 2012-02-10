409a8b92bc729d044d28a0c261009cb6dea7c82b
Utkarsh <Utkarsh@TheTruth.ece.ucsb.edu>
2012-02-10 01:45:07 -0800

    Multiple fixes for 0.51

---
+++ bqcore/bq/blob_service/api.py
@@ -0,0 +1,39 @@
+from bq.core.service import service_registry
+
+from controllers.blobsrv import  guess_type
+from controllers.blob_storage import make_uniq_hash
+
+def find_server():
+    return service_registry.find_service ('blob_service')
+
+def store_blob(filesrc=None, filename=None, url=None, permission='private', **kw):
+    server = find_server()
+    return server.storeBlob(flosrc=filesrc, filename=filename,url=url, permission=permission, **kw)
+
+def localpath(ident):
+    server = find_server()
+    return server.localpath(ident)
+
+def file_exists(ident):
+    server = find_server()
+    return server.fileExists(ident)
+
+def original_name(ident):
+    server = find_server()
+    return server.originalFileName(ident)
+
+def files_exist(hashes):
+    server = find_server()
+    return server.blobsExist(hashes)
+
+
+
+#def fetch(url):
+#    server = find_server(url)
+#    return server.get_one(url)
+
+#def stream(url):
+#    server = find_server(url)
+
+#    blob_path = url.rsplit('/',1)[1]
+#    return server.stream(blob_path)
