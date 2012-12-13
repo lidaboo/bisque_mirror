52ef2a9b2a0d88ab90f96e4c62ec16d12008cb24
Kristian Kvilekval <kris@cs.ucsb.edu>
2012-12-12 16:15:41 -0800

    merge

---
+++ bqserver/bq/export_service/controllers/archiver/archiver_factory.py
@@ -0,0 +1,77 @@
+from cStringIO import StringIO
+from lxml import etree
+import os
+
+class AbstractArchiver():
+
+    def getContentType(self):
+        return 'text/plain'
+
+    def getFileExtension(self):
+        return '.xml'
+    
+    def beginFile(self, file):
+        if file.get('extension') == 'URL':
+            self.reader = StringIO(file.get('content'))
+            self.reader.seek(0, 2)
+            self.fileSize = self.reader.tell()
+            self.reader.seek(0)
+        
+        elif file.get('extension') == '.xml' or file.get('path') is None:
+            file['extension'] = '.xml'  # ensure extension is XML if we are only exporting XML
+            self.reader = StringIO(etree.tostring(file.get('XML')))
+            self.reader.seek(0, 2)
+            self.fileSize = self.reader.tell()
+            self.reader.seek(0)
+
+        else:
+            self.reader = open(file.get('path'), 'rb')
+
+        return
+    
+    def readBlock(self, block_size):
+        return self.reader.read(block_size)
+    
+    def EOF(self):
+        return self.reader.tell()==self.fileSize
+    
+    def endFile(self):
+        self.reader.close()
+        return
+    
+    def readEnding(self):
+        return ''
+    
+    def close(self):
+        return
+    
+    def destinationPath(self, file):
+        uniq = '' #-' + file.get('uniq')[-4:] if file.get('uniq') is not None else '' 
+        path = file.get('dataset') +  '/' if file.get('dataset') is not '' else ''
+        fileName, fileExt = os.path.splitext(file.get('name'))
+        if file.get('extension') == 'URL':
+            return file.get('name')
+        path +=  fileName + uniq + (file.get('extension') or fileExt)
+        return path
+
+
+class ArchiverFactory():
+    
+    from bq.export_service.controllers.archiver.tar_archiver import TarArchiver
+    from bq.export_service.controllers.archiver.zip_archiver import ZipArchiver
+    from bq.export_service.controllers.archiver.gzip_archiver import GZipArchiver
+    from bq.export_service.controllers.archiver.bz2_archiver import BZip2Archiver
+
+    supportedArchivers = {
+                             'tar'  :   TarArchiver,
+                             'zip'  :   ZipArchiver,
+                             'gzip' :   GZipArchiver,
+                             'bz2'  :   BZip2Archiver,
+                         }  
+    
+    @staticmethod
+    def getClass(compressionType):
+        archiver = ArchiverFactory.supportedArchivers.get(compressionType)
+        archiver = AbstractArchiver if archiver is None else archiver  
+
+        return archiver()
