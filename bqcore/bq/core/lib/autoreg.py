21aa71206cff43657f247162561bc73ec8de772b
Utkarsh Gaur <utkarsh.gaur@gmail.com>
2013-07-30 11:30:24 -0700

    Summary : Tickets 599, 714, 770, 781, 787, 791, 792, 793
    1. Tag renderers now work with URIs in addition to strings.
    2. Module name and status displayed in MEX card and full layouts.
    3. ImageMatting only accepts 2D images now.
    4. Increased limit of text fields to Number.MAX_VALUE.
    5. Consolidated the module interface template and resource template.
    6. Fixed code to support ExtJS 4.2.1.
    7. Fixed a race condition bug in resource page view.
    8. Typed value of a tag isn't discarded anymore while the spinner is loading.

---
+++ bqcore/bq/core/lib/autoreg.py
@@ -0,0 +1,88 @@
+import logging
+import transaction
+from sqlalchemy.exc import SQLAlchemyError, DatabaseError
+from zope.interface import implements
+from repoze.who.interfaces import IAuthenticator, IMetadataProvider
+
+from bq.core import model
+
+log = logging.getLogger('bq.auth.autoreg')
+
+class AutoRegister (object):
+    """This plugin attempts to register users that are so far unknown 
+    to the system.  During the metadata phase it looks to see if the user
+    name is currently known and if  not so will create a local user structure
+    """
+    implements(IMetadataProvider)
+
+    key_map = {
+        # maps identity : sreg keys
+        'display_name': 'fullname',
+        #'username': 'nickname',
+        'email_address': 'email',
+    }
+
+
+    def __init__( self, key_map = {} ):
+        """Create autoregister metadata provider to create local users
+        structures.
+
+        """
+        log.debug("autoreg created")
+        self.mapping = {}
+        self.key_map = key_map
+
+    def login_group (self, login_identifier):
+        log.debug ('login_group')
+        g = model.DBSession.query(model.Group).filter_by(group_name = login_identifier).first()
+        if g is None:
+            g = model.Group()
+            g.group_name = login_identifier
+            g.display_name = u'%s Group' % login_identifier 
+            model.DBSession.add(g)
+        return g
+
+
+    def register_user( self, user_name, values = {} ):
+        """Attempt to register the user locally"""
+        name_match = model.User.by_user_name( user_name )
+        if name_match is not None:
+            log.info("found existing user: name %s by user %s " % (user_name, name_match))
+            return user_name
+
+        email_match= values.get('email_address') and model.User.by_email_address(values['email_address'])
+        if email_match is not None:
+            log.info("found existing user: name %s by email %s " % (user_name, email_match))
+            return user_name
+
+        identifier = values.pop('identifier', None)
+        try:
+            log.info("adding user %s with %s" % (user_name, values ))
+                
+            u = model.User(user_name = user_name, 
+                           display_name = values.get('display_name'),
+                           email_address = values.get('email_address'))
+                
+            model.DBSession.add(u)
+            if identifier:
+                g = self.login_group(identifier)
+                g.users.append(u)
+            transaction.commit()
+            return user_name
+        except (SQLAlchemyError, DatabaseError), e:
+            log.exception('problem with autoreg')
+        return None
+
+    def add_metadata( self, environ, identity ):
+        """Add our stored metadata to given identity if available"""
+        user = identity.get('repoze.who.userid', None)
+        log.debug ("metadata with user: %s" % user) 
+        try:
+            from bq.data_service.model import BQUser
+            if user:
+                bquser =  model.DBSession.query (BQUser).filter_by(resource_name = user).first()
+                identity['bisque.bquser'] = bquser
+                log.debug ("adding %s to identity %s" % (bquser, identity))
+        except ImportError:
+            pass
+        return identity
