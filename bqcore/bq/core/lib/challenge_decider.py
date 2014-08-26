435e954dbfac72a0fe51fd32c8601e5880e14241
Dmitry Fedorov <fedorov@ece.ucsb.edu>
2014-08-26 01:52:22 +0100

    UI updates for icons, started move to SVG

---
+++ bqcore/bq/core/lib/challenge_decider.py
@@ -0,0 +1,66 @@
+import itertools
+import logging
+import zope
+
+from webob import Request, Response
+
+from paste.httpheaders import REQUEST_METHOD
+from paste.httpheaders import CONTENT_TYPE
+from paste.httpheaders import USER_AGENT
+from paste.httpheaders import WWW_AUTHENTICATE
+
+from zope.interface import implements
+from repoze.who.interfaces import IChallenger, IIdentifier, IAuthenticator
+from repoze.who.interfaces import IRequestClassifier, IChallengeDecider
+from repoze.who.classifiers import default_request_classifier
+
+log = logging.getLogger('bq.auth.challenge')
+
+def bisque_challenge_decider(environ, status, headers):
+
+    #log.info ('challange_decider')
+    # we do the default if it's a 401, probably we show a form then
+    if status.startswith('401 '):
+        request = Request(environ)
+        response = Response(environ)
+
+        #log.debug ('401 INFO header=%s environ=%s' % (headers, environ))
+
+        req_content = request.headers['content-type']
+        accept = request.headers.get('accept')
+        content_type = response.headers.get('content-type')
+
+        # By default several browser send accept : application/xml
+        # http://www.grauw.nl/blog/entry/470
+        if accept and 'text/xml' in accept: #or 'application/xml' in accept:
+            return False
+
+        if content_type and 'text/xml' in content_type \
+            or 'application/xml' in content_type \
+            or 'text/xml' in req_content \
+            or 'application/xml' in req_content:
+            return False
+        log.debug ('challange requested')
+        log.debug ('req %s resp %s' % (req_content, content_type))
+        return True
+    elif environ.has_key('repoze.whoplugins.openid.openid'):
+        # in case IIdentification found an openid it should be in the environ
+        # and we do the challenge
+        return True
+    elif environ.has_key('repoze.who.plugins.cas'):
+        # in case IIdentification found an cas it should be in the environ
+        # and we do the challenge
+        return True
+    return False
+
+zope.interface.directlyProvides(bisque_challenge_decider, IChallengeDecider)
+
+
+def bisque_request_classifier(environ):
+    content_type = CONTENT_TYPE(environ)
+    if content_type in ('text/xml', 'application/xml'):
+        environ['CLIENT_APP'] = 'clientapp'
+    else:
+        environ['CLIENT_APP'] = default_request_classifier(environ)
+    return environ['CLIENT_APP']
+zope.interface.directlyProvides(bisque_request_classifier, IRequestClassifier)
---
+++ bqserver/bq/image_service/controllers/converter_base.py
@@ -0,0 +1,330 @@
+# converter_base.py
+# Author: Dmitry Fedorov
+# Center for BioImage Informatics, University California, Santa Barbara
+from __future__ import with_statement
+
+""" Base class defining command line converter API
+"""
+
+__module__    = "converter_base"
+__author__    = "Dmitry Fedorov"
+__version__   = "0.1"
+__revision__  = "$Rev$"
+__date__      = "$Date$"
+__copyright__ = "Center for BioImage Informatics, University California, Santa Barbara"
+
+import os.path
+from subprocess import call
+from pylons.controllers.util import abort
+#from collections import OrderedDict
+from bq.util.compat import OrderedDict
+from itertools import groupby
+
+from .locks import Locks
+from . import misc
+
+import logging
+log = logging.getLogger('bq.image_service.converter')
+
+################################################################################
+# Format - the definition of a format
+################################################################################
+
+class Format(object):
+
+    def __init__(self, name='', fullname='', ext=[], reading=False, writing=False, multipage=False, metadata=False, samples=(0,0), bits=(0,0)):
+        self.name      = name
+        self.fullname  = fullname
+        self.ext       = ext
+        self.reading   = reading
+        self.writing   = writing
+        self.multipage = multipage
+        self.metadata  = metadata
+        self.samples_per_pixel_min_max = samples
+        self.bits_per_sample_min_max   = bits
+
+    def supportToString(self):
+        s = []
+        if self.reading   is True:
+            s.append('reading')
+        if self.writing   is True:
+            s.append('writing')
+        if self.multipage is True:
+            s.append('multipage')
+        if self.metadata  is True:
+            s.append('metadata')
+        return ','.join(s)
+
+################################################################################
+# ConverterBase
+################################################################################
+
+class ConverterBase(object):
+    installed = False
+    version = None
+    installed_formats = None # OrderedDict containing Format and keyed by format name
+    CONVERTERCOMMAND = 'convert' if os.name != 'nt' else 'convert.exe'
+
+    #######################################
+    # Init
+    #######################################
+
+    #@staticmethod
+    @classmethod
+    def init(cls):
+        cls.version = cls.get_version()
+        cls.installed = cls.get_installed()
+        if cls.installed is not False:
+            cls.get_formats()
+
+    #######################################
+    # Version and Installed
+    #######################################
+
+    # overwrite with appropriate implementation
+    @classmethod
+    def get_version (cls):
+        '''returns the version of command line utility'''
+        return {
+            'full': '0.0.0',
+            'numeric': [0,0,0],
+            'major': 0,
+            'minor': 0,
+            'build': 0,
+        }
+
+    @classmethod
+    def get_installed (cls):
+        '''Returns true if converter is installed'''
+        if cls.version is not None and 'full' in cls.version:
+            return True
+        return False
+
+    @classmethod
+    def check_version ( cls, needed ):
+        '''checks if converter is of proper version'''
+        if cls.version is None or not 'numeric' in cls.version:
+            return False
+        if isinstance(needed, str):
+            needed = [int(s) for s in needed.split('.')]
+        has = cls.version['numeric']
+        return needed <= has
+
+    @classmethod
+    def ensure_version ( cls, needed ):
+        '''checks if converter is of proper version and sets installed to false if its older'''
+        cls.installed = cls.check_version ( needed )
+        return cls.installed
+
+    #######################################
+    # Formats
+    #######################################
+
+    # overwrite with appropriate implementation
+    @classmethod
+    def get_formats(cls):
+        '''inits supported file formats'''
+        if cls.installed_formats is None:
+            cls.installed_formats = OrderedDict() # OrderedDict containing Format and keyed by format name
+
+    def formats(self):
+        '''return the XML with supported file formats'''
+        return self.installed_formats
+
+    #######################################
+    # Supported
+    #######################################
+
+    # overwrite with appropriate implementation
+    def supported(self, ifnm, **kw):
+        '''return True if the input file format is supported'''
+        return False
+
+
+    #######################################
+    # Meta - returns a dict with all the metadata fields
+    #######################################
+
+    # overwrite with appropriate implementation
+    def meta(self, ifnm, series=0, **kw):
+        '''returns a dict with file metadata'''
+        return {}
+
+    #######################################
+    # The info command returns the "core" metadata (width, height, number of planes, etc.)
+    # as a dictionary
+    #######################################
+
+    # overwrite with appropriate implementation
+    def info(self, ifnm, series=0, **kw):
+        '''returns a dict with file info'''
+        if not self.installed:
+            return {}
+        if not os.path.exists(ifnm):
+            return {}
+
+        rd = self.meta(ifnm, series)
+        core = [ 'image_num_series', 'image_num_x', 'image_num_y', 'image_num_z', 'image_num_c', 'image_num_t',
+                 'image_pixel_format', 'image_pixel_depth',
+                 'pixel_resolution_x', 'pixel_resolution_y', 'pixel_resolution_z',
+                 'pixel_resolution_unit_x', 'pixel_resolution_unit_y', 'pixel_resolution_unit_z' ]
+
+        #return {k:v for k,v in rd.iteritems() if k in core}
+        return dict ( (k,v) for k,v in rd.iteritems() if k in core)
+
+    #######################################
+    # Multi-file misc
+    #######################################
+
+    @classmethod
+    def is_multifile_series(cls, **kw):
+        ''' Test if incoming is a multi-file no-header series like TIFF series '''
+        #log.debug('is_multifile_series kw: %s', kw)
+        
+        # test if token is present
+        try:
+            token = kw['token']
+        except (KeyError):
+            return False
+
+        # test for multiple files
+        try:
+            meta = token.meta
+            #log.debug('is_multifile_series meta: %s', meta)
+            files = meta['files']
+            #log.debug('is_multifile_series files: %s', files)
+            if len(files)<=1:
+                return False
+        except (KeyError, TypeError, AttributeError):
+            return False
+        
+        # test for geometry tags
+        if len(set(['image_num_z','image_num_t','image_num_c']).intersection(meta.keys()))<1:
+            return False
+
+        log.debug('is_multifile_series is True')
+        return True
+
+    @classmethod
+    def enumerate_series_files(cls, **kw):
+        ''' Find all files belonging to a multi-file series '''
+        
+        # test if token is present
+        try:
+            token = kw['token']
+        except (KeyError):
+            return []
+
+        # test for multiple files
+        try:
+            meta = token.meta
+            files = meta['files']
+        except (KeyError, TypeError, AttributeError):
+            return []
+        
+        files = list(set(files)) # ensure unique names
+        files = sorted(files, key=misc.blocked_alpha_num_sort) # use alpha-numeric sort
+        return files
+
+    #######################################
+    # Conversion
+    #######################################
+
+    @classmethod
+    def run_read(cls, ifnm, command ):
+        with Locks(ifnm):
+            command, tmp = misc.start_nounicode_win(ifnm, command)
+            log.debug('run_read command: [%s]', command)
+            out = misc.run_command( command )
+            misc.end_nounicode_win(tmp)
+        return out
+
+    @classmethod
+    def run(cls, ifnm, ofnm, args, **kw ):
+        '''converts input filename into output using exact arguments as provided in args'''
+        if not cls.installed:
+            return None
+        with Locks(ifnm, ofnm) as l:
+            if l.locked: # the file is not being currently written by another process
+                command = [cls.CONVERTERCOMMAND]
+                command.extend(args)
+                log.debug('Run command: [%s]', command)
+                proceed = True
+                if ofnm is not None and os.path.exists(ofnm) and os.path.getsize(ofnm)>16:
+                    if kw.get('nooverwrite', False) is True:
+                        proceed = False
+                        log.warning ('Run: output exists before command [%s], skipping', ofnm)
+                    else:
+                        log.warning ('Run: output exists before command [%s], overwriting', ofnm)
+                if proceed is True:
+                    command, tmp = misc.start_nounicode_win(ifnm, command)
+                    retcode = call (command)
+                    misc.end_nounicode_win(tmp)
+                    if retcode == 99:
+                        # in case of a timeout
+                        log.info ('Run: timed-out for [%s]', command)
+                        if ofnm is not None and os.path.exists(ofnm):
+                            os.remove(ofnm)
+                        abort(412, 'Requested timeout reached')
+                    if retcode!=0:
+                        log.warning ('Run: returned [%s] for [%s]', retcode, command)
+                        return None
+                    if ofnm is None:
+                        return str(retcode)
+                    # output file does not exist for some operations, like tiles
+                    # tile command does not produce a file with this filename
+                    # if not os.path.exists(ofnm):
+                    #     log.error ('Run: output does not exist after command [%s]', ofnm)
+                    #     return None
+
+        # make sure the write of the output file have finished
+        if ofnm is not None and os.path.exists(ofnm):
+            with Locks(ofnm):
+                pass
+
+        # safeguard for incorrectly converted files, sometimes only the tiff header can be written
+        # empty lock files are automatically removed before by lock code
+        if os.path.exists(ofnm) and os.path.getsize(ofnm) < 16:
+            log.error ('Run: output file is smaller than 16 bytes, probably an error, removing [%s]', ofnm)
+            os.remove(ofnm)
+            return None
+        return ofnm
+
+    @classmethod
+    def convert(cls, ifnm, ofnm, fmt=None, series=0, extra=None, **kw):
+        '''converts a file and returns output filename'''
+        command = ['-input', ifnm]
+        if ofnm is not None:
+            command.extend (['-output', ofnm])
+        if fmt is not None:
+            command.extend (['-format', fmt])
+        if series is not None:
+            command.extend (['-series', str(series)])
+        #command.extend (extra)
+        return cls.run(ifnm, ofnm, command )
+
+    # overwrite with appropriate implementation
+    @classmethod
+    def convertToOmeTiff(cls, ifnm, ofnm, series=0, extra=None, **kw):
+        '''converts input filename into output in OME-TIFF format'''
+        return cls.run(ifnm, ofnm, ['-input', ifnm, '-output', ofnm, '-format', 'OmeTiff', '-series', '%s'%series] )
+
+    # overwrite with appropriate implementation
+    @classmethod
+    def thumbnail(cls, ifnm, ofnm, width, height, series=0, **kw):
+        '''converts input filename into output thumbnail'''
+        return cls.run(ifnm, ofnm, ['-input', ifnm, '-output', ofnm, '-format', 'jpeg', '-series', '%s'%series, '-thumbnail'] )
+
+    # overwrite with appropriate implementation
+    @classmethod
+    def slice(cls, ifnm, ofnm, z, t, roi=None, series=0, **kw):
+        '''extract Z,T plane from input filename into output in OME-TIFF format'''
+        #z1,z2 = z
+        #t1,t2 = t
+        #x1,x2,y1,y2 = roi
+        #token = kw.get('token', None)
+
+        return cls.run(ifnm, ofnm, ['-input', ifnm, '-output', ofnm, '-format', 'OmeTiff', '-series', '%s'%series, 'z', '%s'%z, 't', '%s'%t] )
+
+
+#ConverterBase.init()
