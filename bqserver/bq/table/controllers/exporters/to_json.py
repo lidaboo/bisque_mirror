026537aab2454258bf32d23605f53775ffd9cfe0
Christian Lang <clang@ece.ucsb.edu>
2016-03-02 18:03:24 -0800

    temp. removal of provenance graph viewer (for stable)

---
+++ bqserver/bq/table/controllers/exporters/to_json.py
@@ -0,0 +1,120 @@
+###############################################################################
+##  BisQue                                                                   ##
+##  Center for Bio-Image Informatics                                         ##
+##  University of California at Santa Barbara                                ##
+## ------------------------------------------------------------------------- ##
+##                                                                           ##
+##     Copyright (c) 2015 by the Regents of the University of California     ##
+##                            All rights reserved                            ##
+##                                                                           ##
+## Redistribution and use in source and binary forms, with or without        ##
+## modification, are permitted provided that the following conditions are    ##
+## met:                                                                      ##
+##                                                                           ##
+##     1. Redistributions of source code must retain the above copyright     ##
+##        notice, this list of conditions, and the following disclaimer.     ##
+##                                                                           ##
+##     2. Redistributions in binary form must reproduce the above copyright  ##
+##        notice, this list of conditions, and the following disclaimer in   ##
+##        the documentation and/or other materials provided with the         ##
+##        distribution.                                                      ##
+##                                                                           ##
+##     3. All advertising materials mentioning features or use of this       ##
+##        software must display the following acknowledgement: This product  ##
+##        includes software developed by the Center for Bio-Image Informatics##
+##        University of California at Santa Barbara, and its contributors.   ##
+##                                                                           ##
+##     4. Neither the name of the University nor the names of its            ##
+##        contributors may be used to endorse or promote products derived    ##
+##        from this software without specific prior written permission.      ##
+##                                                                           ##
+## THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS "AS IS" AND ANY ##
+## EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED ##
+## WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, ARE   ##
+## DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR  ##
+## ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL    ##
+## DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS   ##
+## OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)     ##
+## HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,       ##
+## STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN  ##
+## ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE           ##
+## POSSIBILITY OF SUCH DAMAGE.                                               ##
+##                                                                           ##
+###############################################################################
+
+"""
+CSV table exporter
+"""
+
+__author__    = "Dmitry Fedorov <dima@dimin.net>"
+__version__   = "1.0"
+__copyright__ = "Center for Bio-Image Informatics, University of California at Santa Barbara"
+
+# default imports
+import os
+import logging
+
+__all__ = [ 'ExporterJSON' ]
+
+log = logging.getLogger("bq.table.export.json")
+
+try:
+    import numpy as np
+except ImportError:
+    log.info('Numpy was not found but required for table service!')
+
+try:
+    import pandas as pd
+except ImportError:
+    log.info('Pandas was not found but required for table service!')
+
+try:
+    import json
+except ImportError:
+    log.info('Json was not found but needed for JSON output...')
+
+from bq.table.controllers.table_exporter import TableExporter
+
+#---------------------------------------------------------------------------------------
+# exporters: Json
+#---------------------------------------------------------------------------------------
+
+class ExporterJSON (TableExporter):
+    '''Formats tables as Json'''
+
+    name = 'json'
+    version = '1.0'
+    ext = 'json'
+    mime_type = 'application/json'
+
+    def info(self, table):
+        super(ExporterJSON, self).info(table)
+        v = {}
+        if table.headers:
+            # has headers => this is a leaf object (table or matrix)
+            v["headers"] = table.headers
+            v["types"] = table.types
+            if table.sizes is not None:
+                v["sizes"] = table.sizes
+        if table.tables is not None:
+            v["group"] = table.tables
+        return json.dumps(v)
+
+    def format(self, table):
+        """ converts table to JSON """
+        #return table.data.to_json()
+        data = table.as_array().tolist()
+        if hasattr(data, "strip") or   \
+           (not hasattr(data, "__getitem__") and   \
+            not hasattr(data, "__iter__")):
+            # data is not a list/tuple => wrap it 
+            data = [ data ]
+        v = {
+            'offset': table.offset,
+            'data': data,
+            'headers': table.headers,
+            'types': table.types,
+        }
+        if table.sizes is not None:
+            v["sizes"] = table.sizes
+        return json.dumps(v)            
---
+++ bqserver/bq/table/controllers/importers/from_excel.py
@@ -0,0 +1,191 @@
+###############################################################################
+##  BisQue                                                                   ##
+##  Center for Bio-Image Informatics                                         ##
+##  University of California at Santa Barbara                                ##
+## ------------------------------------------------------------------------- ##
+##                                                                           ##
+##     Copyright (c) 2015 by the Regents of the University of California     ##
+##                            All rights reserved                            ##
+##                                                                           ##
+## Redistribution and use in source and binary forms, with or without        ##
+## modification, are permitted provided that the following conditions are    ##
+## met:                                                                      ##
+##                                                                           ##
+##     1. Redistributions of source code must retain the above copyright     ##
+##        notice, this list of conditions, and the following disclaimer.     ##
+##                                                                           ##
+##     2. Redistributions in binary form must reproduce the above copyright  ##
+##        notice, this list of conditions, and the following disclaimer in   ##
+##        the documentation and/or other materials provided with the         ##
+##        distribution.                                                      ##
+##                                                                           ##
+##     3. All advertising materials mentioning features or use of this       ##
+##        software must display the following acknowledgement: This product  ##
+##        includes software developed by the Center for Bio-Image Informatics##
+##        University of California at Santa Barbara, and its contributors.   ##
+##                                                                           ##
+##     4. Neither the name of the University nor the names of its            ##
+##        contributors may be used to endorse or promote products derived    ##
+##        from this software without specific prior written permission.      ##
+##                                                                           ##
+## THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS "AS IS" AND ANY ##
+## EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED ##
+## WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, ARE   ##
+## DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR  ##
+## ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL    ##
+## DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS   ##
+## OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)     ##
+## HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,       ##
+## STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN  ##
+## ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE           ##
+## POSSIBILITY OF SUCH DAMAGE.                                               ##
+##                                                                           ##
+###############################################################################
+
+"""
+Excel table importer
+"""
+
+__author__    = "Dmitry Fedorov <dima@dimin.net>"
+__version__   = "1.0"
+__copyright__ = "Center for Bio-Image Informatics, University of California at Santa Barbara"
+
+# default imports
+import os
+import logging
+import pkg_resources
+from pylons.controllers.util import abort
+
+from bq import blob_service
+
+__all__ = [ 'TableExcel' ]
+
+log = logging.getLogger("bq.table.import.excel")
+
+try:
+    import numpy as np
+except ImportError:
+    log.info('Numpy was not found but required for table service!')
+
+try:
+    import xlrd
+except ImportError:
+    log.info('Xlrd was not found but required for Excel tables!')
+
+try:
+    import pandas as pd
+except ImportError:
+    log.info('Pandas was not found but required for table service!')
+
+from bq.table.controllers.table_base import TableBase
+
+################################################################################
+# misc
+################################################################################
+
+def extjs_safe_header(s):
+    if isinstance(s, basestring):
+        return s.replace('.', '_')
+    return s
+
+def _get_headers_types(data, startcol=None, endcol=None):
+    headers = [extjs_safe_header(x) for x in data.columns.values.tolist()[slice(startcol, endcol, None)]] # extjs errors loading strings with dots
+    types = [t.name for t in data.dtypes.tolist()[slice(startcol, endcol, None)]] #data.dtypes.tolist()[0].name
+    return (headers, types)
+            
+#---------------------------------------------------------------------------------------
+# Importer: Excel
+# TODO: identify if header is present
+# TODO: only read the requested number of rows
+#---------------------------------------------------------------------------------------
+
+class TableExcel(TableBase):
+    '''Formats tables into output format'''
+
+    name = 'excel'
+    version = '1.0'
+    ext = ['xls', 'xlsx']
+    mime_type = 'application/vnd.ms-excel'
+
+    def __init__(self, uniq, resource, path, **kw):
+        """ Returns table information """
+        super(TableExcel, self).__init__(uniq, resource, path, **kw)
+
+        # try to load the resource binary
+        b = blob_service.localpath(uniq, resource=resource) or abort (404, 'File not available from blob service')
+        self.filename = b.path
+        self.t = None
+        self.info()
+
+    def close(self):
+        """Close table"""
+        if self.t:
+            self.t.close()
+
+    def info(self, **kw):
+        """ Returns table information """
+        # load headers and types if empty
+
+        if self.tables is None:
+            try:
+                self.t = pd.ExcelFile(self.filename)
+            except Exception:
+                raise RuntimeError("Excel file cannot be read")
+            self.tables = [ { 'path':name, 'type':'sheet' } for name in self.t.sheet_names ]
+
+        if len(self.tables)==1: # if only one sheet is present
+            self.subpath = self.tables[0]['path']
+            if len(self.path)>0 and self.path[0] == self.subpath:
+                self.path.pop(0)
+        elif len(self.path)>0 and self.path[0] in [tab['path'] for tab in self.tables]: # if path is provided for a sheet
+            self.subpath = self.path.pop(0)
+        else: # if no path is provided, use first sheet
+            self.subpath = self.tables[0]['path']
+
+        data = pd.read_excel(self.t, self.subpath, nrows=1)
+        self.headers, self.types = _get_headers_types(data)
+        self.sizes = data.shape
+        log.debug('Excel types: %s, header: %s, sizes: %s', str(self.types), str(self.headers), str(self.sizes))
+        return { 'headers': self.headers, 'types': self.types, 'sizes': self.sizes }
+
+    def read(self, **kw):
+        """ Read table cells and return """
+        super(TableExcel, self).read(**kw)
+        rng = kw.get('rng')
+        log.debug('rng %s', str(rng))
+
+        data = pd.read_excel(self.t, self.subpath, nrows=1)   # to get the shape later
+        startrows = [0]*2
+        endrows   = [min(50, data.shape[i]) for i in range(2)]
+        if rng is not None:
+            for i in range(min(2, len(rng))):
+                row_range = rng[i]
+                if len(row_range)>0:
+                    startrows[i] = row_range[0] if len(row_range)>0 and row_range[0] is not None else 0
+                    endrows[i]   = row_range[1]+1 if len(row_range)>1 and row_range[1] is not None else data.shape[i]
+                    startrows[i] = min(data.shape[i], max(0, startrows[i]))
+                    endrows[i]   = min(data.shape[i], max(0, endrows[i]))
+                    if startrows[i] > endrows[i]:
+                        endrows[i] = startrows[i]        
+        log.debug('startrows %s, endrows %s', startrows, endrows)
+        
+        usecols = range(startrows[1], endrows[1])
+        if endrows[0] > startrows[0] and endrows[1] > startrows[1]:
+            self.data = pd.read_excel(self.t, self.subpath, skiprows=startrows[0], nrows=endrows[0]-startrows[0], parse_cols=usecols)
+        else:
+            self.data = pd.DataFrame()   # empty table
+        # excel cannot read only a specified number of rows, select now
+        self.data = self.data[0:endrows[0]-startrows[0]]
+        self.sizes = [endrows[i]-startrows[i] for i in range(self.data.ndim)]
+        log.debug('Data: %s', str(self.data.head()) if self.data.ndim > 0 else str(self.data))
+        self.headers, self.types = _get_headers_types(data, startrows[1], endrows[1])
+        return self.data
+
+    def write(self, data, **kw):
+        """ Write cells into a table"""
+        abort(501, 'Excel write not implemented')
+
+    def delete(self, **kw):
+        """ Delete cells from a table"""
+        abort(501, 'Excel delete not implemented')
+
