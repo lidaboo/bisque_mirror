3a69132f366e8563a19f46b6c591446382c7256e
Dmitry Fedorov <dima@dimin.net>
2017-05-23 11:52:30 -0600

    install fixes

---
+++ bqcore/bq/core/public/plugins/pipeline/pipeline.py
@@ -0,0 +1,41 @@
+from bq.blob_service.controllers.blob_plugins import ResourcePlugin
+from bq.pipeline.controllers.importers.from_cellprofiler import upload_cellprofiler_pipeline
+
+class Dream3DPipelinePlugin (ResourcePlugin):
+    '''Dream.3D Pipeline resource''' 
+    name = "PipelinePlugin"  
+    version = '1.0'
+    ext = 'json'
+    resource_type = 'dream3d_pipeline'
+    mime_type = 'application/json'
+    
+    def __init__(self):
+        pass
+
+class CellprofilerPipelinePlugin (ResourcePlugin):
+    '''Cellprofiler Pipeline resource''' 
+    name = "PipelinePlugin"  
+    version = '1.0'
+    ext = 'cp'
+    resource_type = 'cellprofiler_pipeline'
+    mime_type = 'text/plain'
+    
+    def __init__(self):
+        pass
+    
+    def process_on_import(self, f, intags):
+        return upload_cellprofiler_pipeline(f, intags)
+
+class CellprofilerPipeline2Plugin (ResourcePlugin):
+    '''Cellprofiler Pipeline resource''' 
+    name = "PipelinePlugin"  
+    version = '1.0'
+    ext = 'cppipe'
+    resource_type = 'cellprofiler_pipeline'
+    mime_type = 'text/plain'
+    
+    def __init__(self):
+        pass
+
+    def process_on_import(self, f, intags):
+        return upload_cellprofiler_pipeline(f, intags)
---
+++ contrib/bisque_paths/setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup
+setup(name='bisque_paths',
+      version='1.0',
+      install_requires = [
+        'requests',
+        'argparse',
+        'six',
+        ],
+
+      py_modules = ['bisque_paths' ],
+
+      entry_points = {
+        'console_scripts' : [
+            'bqpath = bisque_paths:main',
+            ]
+        }
+
+      )
